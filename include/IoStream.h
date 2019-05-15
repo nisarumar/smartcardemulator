4ab0ddaecbdd3e9ebd8c2df17039fe051b82cba3
Umar Nisar <umar.nisar@tum.de>
2019-05-15 21:18:10 +0200

    add IoStream module to link standard IO streams to UART
    The printf skips some letter, this is by design to not wait so
    long for character printing.

---
+++ include/IoStream.h
@@ -0,0 +1,12 @@
+#ifndef IOSTREAM_H_
+#define IOSTREAM_H_
+
+/*
+ *
+ * name: IOStream_LinkStream
+ * @param	void
+ * @return	void
+ * @brief: links std out and err with USART_PutChar
+ */
+void IoStream_LinkStream(void);
+#endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
-#include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "IO.h"
+#include "IoStream.h"
 
 int main(void)
 {
 // Configure pin 7 on port A as output
 	DDRA |= (1 << PINA7); // Debug-LED
 // Infinite loop
+	IoStream_LinkStream();
 	while(1)
 	{
+		printf("Hello\n");
 	// Invert the output of pin 7 on port A
 	PORTA ^= (1 << PINA7);
-	_delay_ms(250);
+	_delay_ms(1000);
 	}
 }
