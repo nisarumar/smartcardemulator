0fbe7141a9636694118dc0c781084cf64c2f7bb6
Chander Kumar <ge73caq@mytum.de>
2019-06-02 15:15:20 +0200

    gf(256) multliplication is implemented using lookup tables

--- smartcard_emulator/include/AES.h
+++ smartcard_emulator/include/AES.h
@@ -5,13 +5,15 @@ void inverse_shift_rows(uint8_t* stateTextText);
 //uint8_t gf256mul(uint8_t in1, uint8_t in2);
 void inverse_mix_coloumns(uint8_t* s);
 //void gen_roundkey(uint8_t* key , uint8_t* k, const uint8_t* sboxarr, const uint8_t* rcon);
-uint8_t xtime(uint8_t x);
+//uint8_t xtime(uint8_t x);
 void aes_dec_128(uint8_t* state ,const uint8_t* roundkeyarray );
-
-extern const uint8_t aes_sbox[];
+uint8_t gf256mul(uint8_t a,uint8_t b);
+extern const uint8_t gf_log_table[];
+extern const uint8_t gf_antilog_table[];
+//extern const uint8_t aes_sbox[];
 extern const uint8_t aes_invsbox[];
-extern const uint8_t rcon []; 
-extern uint8_t	stateText [];
+//extern const uint8_t rcon[]; 
+extern uint8_t	stateText[];
 extern const uint8_t roundkeyarr[];
 //extern uint8_t key[];
 
