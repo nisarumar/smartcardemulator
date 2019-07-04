1815f542b3fc3309f342b45b29308de5d7734408
Umar Nisar <umar.nisar@tum.de>
2019-07-04 23:27:56 +0200

    add grpahs for autocorr

--- smartcard_emulator/scripts/randomAnalysis.py
+++ smartcard_emulator/scripts/randomAnalysis.py
@@ -2,7 +2,7 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 logging.basicConfig(level=logging.DEBUG)
-filename="putty.log"
+filename="putty.log2"
 filehndl= open(filename,'r');
 randarrRaw = []
 randarrRef = []
@@ -17,7 +17,23 @@ for line in filehndl:
 print("Size is: %d"%len(randarrRaw))
 print("Size is: %d"%len(randarrRef))
 npArrRaw = np.array(randarrRaw)
+#npArrRaw=np.arange(1000)
+k=128
 npArrRef = np.array(randarrRef)
+Rawacf = 1/sum(npArrRaw) * np.correlate(npArrRaw, npArrRaw, mode='full')
+Rawacf = Rawacf[(len(npArrRaw)-1)-(k-1):(len(npArrRaw)-1)+k]
+Refacf = 1/sum(npArrRef) * np.correlate(npArrRef, npArrRef, mode='full')
+Refacf = Refacf[(len(npArrRef)-1)-(k-1):(len(npArrRef)-1)+k]
+kappa = np.arange(-(k-1), k)
+plt.figure(2)
+plt.subplot(211)
+plt.plot(kappa,Rawacf)
+plt.xlabel('Lags')
+plt.ylabel('AutoCorrelation of Raw Entropy')
+plt.subplot(212)
+plt.plot(kappa,Refacf)
+plt.xlabel('Lags')
+plt.ylabel('AutoCorrelation of PRNG')
 plt.figure(1)
 plt.subplot(211)
 plt.hist(npArrRaw, bins=256)
@@ -27,7 +43,7 @@ plt.subplot(212)
 plt.hist(npArrRef, bins=256)
 plt.xlabel('PRNG Bytes')
 plt.ylabel('Frequency')
-#plt.show()
+plt.show()
 
 
 
